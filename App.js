9b8d8fda9747f0c4b5f679f0990c047deadea229
John Schoeman <johnschoeman1617@gmail.com>
2019-02-18 10:02:24 -0500

    Add splash and icon pngs
    Why:
    To publish we will need a splash screen and an app icon asset
    
    This commit:
    add those assets to the project

--- App.js
+++ App.js
@@ -29,10 +29,7 @@ class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
+      Asset.loadAsync([]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
@@ -57,7 +54,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
   },
 })
 

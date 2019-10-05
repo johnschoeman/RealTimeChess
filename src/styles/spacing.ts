0320b9401e9c0fab0dc21390aa43415df82dc943
Jeff Stolz <jstolz123@gmail.com>
2019-10-04 20:32:53 -0400

    restyle welcome screen (#11)
    Why:
    So we can have a zany welcome screen that sets the tone of the app and
    is consistent with the rest of the brand.
    
    This commit:
    Removes copy, adds a background image, and restyles the play button.

--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,5 +1,8 @@
 export const base = 10
 export const medium = 20
+export const large = 30
+export const xLarge = 40
+export const huge = 70
 
 export const tileWidth = base
 export const tileHeight = base

73fac7148de977e1a31f76415ad87e1cabbf4c65
Jeff Stolz <jstolz123@gmail.com>
2019-10-07 20:25:02 -0400

    style status bar (#21)
    Why:
    The status bar needs to blend in with and match the rest of the
    application. A custom component is needed for iOS as it doesn't support
    the react native backgroundColor.
    
    This commit:
    Creates a component for the status bar and renders it in the
    application.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,44 @@
 import React from "react"
-import { SafeAreaView } from "react-native"
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native"
 
 import { GameProvider } from "./GameContext"
 import AppNavigatorContainer from "./AppNavigatorContainer"
 
+import { Color } from "./styles"
+
+const GeneralStatusBar = () => (
+  <View style={[styles.statusBar, { backgroundColor: Color.black }]}>
+    <StatusBar
+      translucent
+      backgroundColor={Color.black}
+      barStyle={"light-content"}
+    />
+  </View>
+)
+
 const RealTimeChess = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <GameProvider>
-        <AppNavigatorContainer />
-      </GameProvider>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <GeneralStatusBar />
+      <SafeAreaView style={{ flex: 1 }}>
+        <GameProvider>
+          <AppNavigatorContainer />
+        </GameProvider>
+      </SafeAreaView>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  statusBar: {
+    height: Platform.OS === "ios" ? 20 : StatusBar.currentHeight,
+  },
+})
 
 export default RealTimeChess
